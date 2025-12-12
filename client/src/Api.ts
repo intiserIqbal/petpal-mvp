c159281670382ad4a650dfd5db9876b85f2e6177
FarjanaAkhi <akhimim693@gmail.com>
2025-12-12 22:13:59 +0600

    done

--- client/src/Api.ts
+++ client/src/Api.ts
@@ -1,1 +1,3 @@
-export const API = "http://localhost:5000";
+// client/src/api.ts
+export const API_URL = import.meta.env.VITE_API_URL;
+export const ADMIN_SECRET = import.meta.env.VITE_ADMIN_SECRET;
